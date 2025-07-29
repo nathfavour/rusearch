78cf24b79ffdc6e4d152868e00021b75f1595be2
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-07-29 22:42:04 +0000

    Allow single positional argument as pattern for recursive search shortcut

--- src/args.rs
+++ src/args.rs
@@ -2,6 +2,11 @@ use clap::Parser;
 use std::fs;
 
 /// Fast recursive directory search CLI
+///
+/// Usage:
+///   rusearch --pattern foo
+///   rusearch --pattern-file file.txt
+///   rusearch "foo"   # shortcut: searches current directory recursively for 'foo'
 #[derive(Parser, Debug)]
 #[command(author, version, about = "Fast recursive directory search CLI", long_about = None)]
 pub struct Args {
@@ -16,15 +21,20 @@ pub struct Args {
     /// File containing pattern to search for
     #[arg(long, required = false)]
     pub pattern_file: Option<String>,
+
+    /// Shortcut: pattern as a single positional argument
+    #[arg(required = false, index = 1, help = "Shortcut: search for this pattern in the current directory recursively if no --pattern or --pattern-file is given")] 
+    pub shortcut_pattern: Option<String>,
 }
 
 impl Args {
     pub fn get_pattern(&self) -> Result<String, String> {
-        match (&self.pattern, &self.pattern_file) {
-            (Some(p), None) => Ok(p.clone()),
-            (None, Some(f)) => fs::read_to_string(f).map_err(|e| format!("Failed to read pattern file: {}", e)),
-            (Some(_), Some(_)) => Err("Provide either --pattern or --pattern-file, not both.".to_string()),
-            (None, None) => Err("You must provide --pattern or --pattern-file.".to_string()),
+        match (&self.pattern, &self.pattern_file, &self.shortcut_pattern) {
+            (Some(p), None, _) => Ok(p.clone()),
+            (None, Some(f), _) => fs::read_to_string(f).map_err(|e| format!("Failed to read pattern file: {}", e)),
+            (Some(_), Some(_), _) => Err("Provide either --pattern or --pattern-file, not both.".to_string()),
+            (None, None, Some(s)) => Ok(s.clone()),
+            (None, None, None) => Err("You must provide --pattern, --pattern-file, or a positional pattern argument.".to_string()),
         }
     }
 }
